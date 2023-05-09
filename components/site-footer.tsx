61ccbf8e703b3638b21e1bbaae206f4b6f767958
Filipe Veronezi <filipeseidi@hotmail.com>
2023-05-09 20:07:33 -0300

    feat: new footer data and style

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -3,37 +3,45 @@ import Link from "next/link"
 export function SiteFooter() {
   return (
     <footer className="sticky top-0 z-10 w-full border-t bg-background">
-      <div className="container flex h-20 items-center justify-center text-center text-sm">
-        <span className="font-medium">
-          ⚡ Built with{" "}
-          <Link
-            href="https://nextjs.org"
-            className="underline underline-offset-2 lg:no-underline lg:hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Next.js
-          </Link>
-          ,{" "}
-          <Link
-            href="https://ui.shadcn.com/"
-            className="underline underline-offset-2 lg:no-underline lg:hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            shadcn/ui
-          </Link>{" "}
-          and{" "}
-          <Link
-            href="https://www.abibliadigital.com.br"
-            className="underline underline-offset-2 lg:no-underline lg:hover:underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ABíbliaDigital
-          </Link>
-        </span>
-      </div>
+      <p className="container py-5 text-center text-sm leading-loose">
+        Built with{" "}
+        <Link
+          href="https://nextjs.org"
+          className="font-medium underline underline-offset-[5px]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Next.js
+        </Link>
+        ,{" "}
+        <Link
+          href="https://ui.shadcn.com/"
+          className="font-medium underline underline-offset-[5px]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          shadcn/ui
+        </Link>{" "}
+        and{" "}
+        <Link
+          href="https://www.abibliadigital.com.br"
+          className="font-medium underline underline-offset-[5px]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ABíbliaDigital
+        </Link>
+        . Designed by{" "}
+        <Link
+          href="https://pacy.co"
+          className="font-medium underline underline-offset-[5px]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pacy
+        </Link>
+        {"."}
+      </p>
     </footer>
   )
 }
